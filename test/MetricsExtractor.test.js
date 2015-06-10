1ab3c5f148b94cd964bd8fbb3d4a094a588eb664
Eduardo Pereira de Sousa <edupsousa@gmail.com>
2015-06-10 10:15:53 -0300

    Created component MetricsExtractor

---
+++ test/MetricsExtractor.test.js
@@ -0,0 +1,81 @@
+var MetricsExtractor = require('../lib/MetricsExtractor.js'),
+    expect = require('chai').expect;
+
+describe('MetricsExtractor', function() {
+    var testRepositoryPath = require('path').join(__dirname,'../');
+    var metricsExtractor;
+
+    beforeEach(function(done) {
+        MetricsExtractor.getExtractorForRepository(testRepositoryPath)
+            .then(function(extractor) {
+                metricsExtractor = extractor;
+            }).done(done);
+    });
+
+    it('Get all commits and his authors from repository', function(done) {
+        metricsExtractor.getCommitsAndAuthors()
+            .then(function(result) {
+                expect(result).to.have.all.keys(['authors','commits']);
+                expect(result.commits).to.be.an('array').with.length.above(0);
+                expect(result.authors).to.be.an('array').with.length.above(0);
+            }).done(done);
+    });
+
+    context('Methods that depends on a commit list', function() {
+        var commitList;
+
+        before(function(done) {
+           metricsExtractor.getCommitsAndAuthors()
+               .then(function(result) {
+                   commitList = result.commits;
+               }).done(done);
+        });
+
+        it('Get files from commit list', function() {
+            metricsExtractor.getFilesFromCommits(commitList)
+                .then(function(result) {
+                    expect(result).to.have.all.keys(['entries', 'files', 'paths']);
+                });
+        });
+
+    });
+
+    context('Methods that depends on a file entry list', function() {
+        var entryList;
+
+        before(function(done) {
+            metricsExtractor.getCommitsAndAuthors()
+                .then(function(result) {
+                    return metricsExtractor.getFilesFromCommits(result.commits);
+                })
+                .then(function(result) {
+                    entryList = result.entries;
+                })
+                .done(done);
+        });
+
+        it('Get metrics for files in entry list', function(done) {
+            metricsExtractor.getMetricsForEntries(entryList)
+                .then(function(metrics) {
+                    expect(metrics).to.be.an('array').with.length.above(0);
+                    for(var i = 0; i < metrics.length; i++) {
+                        var fileMetric = metrics[i];
+                        expect(fileMetric).to.have.property('id')
+                            .that.is.a('string');
+                        expect(fileMetric).to.have.property('error')
+                            .that.is.a('boolean');
+                        if (fileMetric.error) {
+                            expect(fileMetric).to.have.property('reason')
+                                .that.is.a('string');
+                        } else {
+                            expect(fileMetric).to.have.property('data')
+                                .that.is.an('object');
+                        }
+                    }
+                })
+                .done(done);
+        });
+    });
+
+
+});
