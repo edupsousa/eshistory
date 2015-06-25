8b0c795ca939d1afe0a63ca87a808a33c3ec7310
Eduardo Pereira de Sousa <edupsousa@gmail.com>
2015-06-24 21:50:09 -0300

    Command refactoring and cleanup

--- bin/js-project-metrics.js
+++ bin/js-project-metrics.js
@@ -1,3 +1,20 @@
 #! /usr/bin/env node
-var command = require('../lib/commands/ProjectMetricsCommand.js');
-command.run();
+var commander = require("commander"),
+    metricsCommand = require("../lib/commands/ProjectMetricsCommand.js"),
+    config = {};
+
+commander
+    .version("0.0.1")
+    .option("-v, --verbose", "Verbose output (to stderr)")
+    .usage("[options] <repository> <output-file>")
+    .parse(process.argv);
+
+if (commander.args.length !== 2) {
+    commander.help();
+    return 1;
+}
+
+if (commander.verbose)
+    config.verbose = true;
+
+metricsCommand.run(commander.args[0], commander.args[1], config);
